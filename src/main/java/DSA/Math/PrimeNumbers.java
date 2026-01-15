26936657813e11e457e939b0f517e55b5473a21c
niyamat <niyamatbarote@gmail.com>
2026-01-15 21:22:38 +0530

    Prime Numbers

--- src/main/java/DSA/Math/PrimeNumbers.java
+++ src/main/java/DSA/Math/PrimeNumbers.java
@@ -48,27 +48,50 @@ public class PrimeNumbers {
     public static List<Integer> primeNums(int n) {
         List<Integer> ans = new ArrayList<>();
         for (int i = 2 ; i < n; i++) {
-            if (isPrimeOpt(i)==true ) {
+            if (isPrimeOpt(i)==true) {
                 ans.add(i);
             }
         }
         return ans;
     }
 
+    // Sieve of Eratosthenes Method
     // if primes[i] is FALSE then it is a prime number
     static void sieve(int n , boolean[] primes) {
-        for (int i = 2; i*i <= n; i++) {
+        for (int i = 2; i*i <= n; i++) {    // i*i <= n (Optimal Condition
             if (!primes[i]) {   // if (primes[2] == false)
-                for (int j = i*2; j <= n; j+=i) {
+                for (int j = i*2; j <= n; j+=i) {   // To make multiple of 2 as TRUE == Non-Prime
                     primes[j] = true;
                 }
             }
         }
+        // Printing the prime Nums(False ones) "index number"
         for (int i = 2; i <= n; i++) {
             if (!primes[i]) {   // if (primes[2] == false)
                 System.out.print(i+" ");
             }
         }
+    }
+
+    // LeetCode Q) 204
+    // https://leetcode.com/problems/count-primes/description/
+    static int eratosthenisCountPrimes(int n) {
+        boolean[] arr = new boolean[n+1];
+
+        for (int i = 2; i*i <= n; i++) {
+            if (!arr[i]) {
+                for (int j = i*2; j <= n; j+=i) {
+                    arr[j] = true;
+            }
+        }
+        }
+        int ans = 0;
+        for (int i = 1; i <= n ; i++) {
+            if (!arr[i]) {
+                ans++;
+            }
+        }
+       return ans;
     }
 
     public static void main(String[] args) {
