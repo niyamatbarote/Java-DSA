592905f0bea670bcde88a0256888e4ba1e443199
niyamat <niyamatbarote@gmail.com>
2026-01-17 23:34:30 +0530

    got answers in sorted manner

--- src/main/java/DSA/Math/Factors.java
+++ src/main/java/DSA/Math/Factors.java
@@ -1,4 +1,35 @@
 package DSA.Math;
 
+import java.util.ArrayList;
+
 public class Factors {
+
+    static void factors1(int n) {
+        for (int i = 1; i <= n; i++) {
+            if (n%i ==0) {
+                System.out.print(i+" ");
+            }
+        }
+    }
+
+    static void factors2(int n) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        for (int i = 1; i <= Math.sqrt(n); i++) {
+            if (n%i==0) {
+                if (n/i == i) {
+                    System.out.print(i+" ");
+                }else {
+                    System.out.print(i +" ");
+                    ans.add(n/i);
+                }
+            }
+        }
+        for (int i = ans.size()-1; i >= 0 ; i--) {
+            System.out.print(ans.get(i)+" ");
+        }
+    }
+
+    public static void main(String[] args) {
+        factors2(100);
+    }
 }
