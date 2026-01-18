554713d7c19f0599b7ea2c2b20a3b9ce5f59109f
niyamat <niyamatbarote@gmail.com>
2026-01-18 16:38:07 +0530

    Found the GCD of an Arrya

--- src/main/java/DSA/Math/HCF_GCD.java
+++ src/main/java/DSA/Math/HCF_GCD.java
@@ -2,6 +2,36 @@ package DSA.Math;
 
 public class HCF_GCD {
 
+    static int maxNumInArray(int[] arr) {
+        int max = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]>max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    static int minNumInArray(int[] arr) {
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]<min) {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+
+    // LC Q) 1979 Link:
+    // https://leetcode.com/problems/find-greatest-common-divisor-of-array/
+    public static int findGCD(int[] arr) {
+        int a = minNumInArray(arr);
+        int b = maxNumInArray(arr);
+
+        return gcd(a,b);
+
+    }
+
     // Euclidean's Formula:-
     static int gcd(int a , int b ) {
 
@@ -13,8 +43,13 @@ public class HCF_GCD {
         return gcd(b%a, a);
     }
 
+
     public static void main(String[] args) {
-        System.out.println(gcd(18,2348));
+        System.out.println(gcd(2,5));
+
+        int[] arr = {353,403,475,371,174,102,406,445,204,485,400};
+        System.out.println(findGCD(arr));
+
 
     }
 }
