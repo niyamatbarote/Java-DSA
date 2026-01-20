77ceabcde3cbfa8322a80326440909cb16979c0a
niyamat <niyamatbarote@gmail.com>
2026-01-20 11:53:16 +0530

    sum of Digits using Recursion

--- src/main/java/DSA/Recursion/Recursion_LVL1.java
+++ src/main/java/DSA/Recursion/Recursion_LVL1.java
@@ -30,11 +30,51 @@ public class Recursion_LVL1 {
         System.out.print(n+" ");
         bothFxn(n-1);
         System.out.print(n+" ");
-
     }
 
+    static double factorial(int n) {
+        // Base Case:
+        if (n < 2) {
+            return n;
+        }
+        // Recursion :
+        return n * factorial(n-1);
+    }
+
+    static int sumOfNdigits(int n) {
+        // Base Case:-
+        if (n <= 1) {
+            return 1;
+        }
+        // Recursive Part :-
+        return n + sumOfNdigits(n-1);
+    }
+
+    // 231 == 2 + 3 + 1 == 6
+    // Using General Method
+    static int sumOfDigits(int n) {
+        int sum = 0;
+        while (n != 0) {
+            // Extract last digit of n (eg:- 232 % 10 == 3)
+            sum += n%10;
+            // Remove last digit of n (e.g:- 232 / 10 == 23)
+            n /= 10;
+        }
+        return sum;
+    }
+
+    static int sumOfDigitsRec(int n) {
+        // Base Case :
+        if (n < 1) {
+            return 0;
+        }
+        // Recursion Formula :-
+        return (n%10) + sumOfDigitsRec(n/10);
+    }
 
     public static void main(String[] args) {
-        bothFxn(10);
+//        bothFxn(10);
+        System.out.println(sumOfDigits(232));
+        System.out.println(sumOfDigitsRec(232));
     }
 }
