390d85cc8c20ebf8280887df9c5c5f9044c0fc67
niyamat <niyamatbarote@gmail.com>
2026-01-20 10:28:28 +0530

    recursion to print from 1 to N && N to 1

--- src/main/java/DSA/Recursion/Recursion_LVL1.java
+++ src/main/java/DSA/Recursion/Recursion_LVL1.java
@@ -1,29 +1,40 @@
 package DSA.Recursion;
 
-
-
 public class Recursion_LVL1 {
 
-    static void revNum(int n) {
+    static void nToOne(int n) {
 
         if (n == 0) {
             return;
         }
         System.out.print(n+" ");
-        revNum(n-1);
+        nToOne(n-1);    // eg :- f(5)->f(4)->f(3)->f(2)->f(1)->f(0)
+        // In this it is printing "n" when stack is being Filled
     }
 
     static void oneToN(int n) {
-        int i = 1;
-        if (i == n) {
+        if (n == 0) {
+            return;
+        }
+        oneToN(n-1);
+        System.out.print(n+" ");
+        // f(5)->f(4)->f(3)->f(2)->f(1)->f(0) (How Stack Is Filling)
+        // return f(0)->f(1)->f(2)->f(3)->f(4)->f(5)    (How Stack is Being Empty)
+        // In this it is printing "n" when stack is being empty
+    }
+
+    static void bothFxn(int n) {
+        if (n == 0) {
             return;
         }
         System.out.print(n+" ");
-        oneToN(n+1);
+        bothFxn(n-1);
+        System.out.print(n+" ");
+
     }
 
 
     public static void main(String[] args) {
-        revNum(10);
+        bothFxn(10);
     }
 }
