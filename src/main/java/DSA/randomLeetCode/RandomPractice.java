0ea2340ef5dc7ea178d4743562f3cb85ebb211fd
niyamat <niyamatbarote@gmail.com>
2026-02-16 11:45:38 +0530

    array revesrsed

--- src/main/java/DSA/randomLeetCode/RandomPractice.java
+++ src/main/java/DSA/randomLeetCode/RandomPractice.java
@@ -1,11 +1,36 @@
 package DSA.randomLeetCode;
 
+import java.util.Arrays;
+
 public class RandomPractice {
 
+    // Swap Elements :
+    public static int[] swap(int[] arr, int start, int end ) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        return arr;
+    }
+
+// Reverse an array :
+    public static int[] rev(int[] arr) {
+        int start = 0;
+        int end = arr.length-1;
+
+        while (start < end) {
+            swap(arr,start,end);
+            start++;
+            end--;
+        }
+        return arr;
+    }
 
 
     public static void main(String[] args) {
-        String s = "ab";
-        char ch = 'c';
+        int[] arrray = {10,20,30,40,50};
+
+        int[] ans = rev(arrray);
+
+        System.out.println(Arrays.toString(ans));
     }
 }
