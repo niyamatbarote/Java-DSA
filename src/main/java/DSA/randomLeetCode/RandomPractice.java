501c36c549ef7447de093250c89dfd15530c6cf1
niyamat <niyamatbarote@gmail.com>
2026-07-29 22:40:55 +0530

    Peak index element finding program

--- src/main/java/DSA/randomLeetCode/RandomPractice.java
+++ src/main/java/DSA/randomLeetCode/RandomPractice.java
@@ -73,218 +73,31 @@ public class RandomPractice {
         return median;
     }
 
-    public static void starPattern1 (int n) {
-
-        for (int i = 1; i<=n; i++) {
-            for (int j = 1; j<= i; j++) {
-                System.out.print(" * ");
-            }
-            System.out.println();
-        }
-    }
-
-//    static void starPattern2 (int n) {
-//        int var = n-1;
-//        for (int i =1 ; i<=n; i++) {
-//
-//            for (int j = var; j<=n; j--) {
-//                System.out.print("   ");
-//            }
-//
-//            for (int k = 1; k<=i; k++) {
-//                System.out.print(" * ");
-//            }
-//            System.out.println();
-//        }
-//    }
-
-    public static boolean isEven(int n) {
-        return n % 2 == 0;
-    }
-
-    public static int binarySearch(int[] arr, int target) {
+    public static int peakElement(int[] arr) {
         int n = arr.length;
         int start = 0;
         int end = n-1;
 
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            // Target Found ..!!
-            if (arr[mid] == target) {
-                return mid;
-            }
-            // Target > Mid
-            if (target > arr[mid]) {
-                start = mid + 1;
-            }
-            // Target < Mid
-            if (target < arr[mid]) {
-                end = mid -1;
+        while (start<end) {
+            int mid= start+(end-start) / 2;
+            // Ascending side :
+            if (arr[mid] < arr[mid+1]) {
+                start = mid +1 ;
+            }else {
+                end = mid;
             }
         }
-        // If Target Not Found
-        return -1;
+        return end;
     }
 
-    public static int countDigits(int n) {
-        if (n == 0) {
-            return 1;
-        }
-// For -ve Numbers
-        if (n < 0) {
-            n = -n;
-        }
 
-        int count = 0;
-        while (n > 0) {
-            n /= 10;
-            count ++;
-        }
-        return count;
-    }
-
-    public static int reverseNum(int n) {
-
-        if (n == 0) {
-            return 1;
-        }
-
-        int rev = 0;
-
-        while (n>0) {
-            int digit = n%10;
-            rev = rev * 10 + digit;
-            n /= 10;
-        }
-        return rev;
-    }
-
-    public static boolean isPalindrome(int n) {
-        int og = n;
-        int rev = 0;
-
-        while (n>0) {
-            int digit = n % 10;
-            rev = rev * 10 + digit;
-            n /= 10;
-        }
-        return og == rev;
-    }
-
-    public static int largestDigit(int n) {
-        if (n<10) {
-            return n;
-        }
-        int large = n % 10;
-
-        while (n>0) {
-            int dig = n % 10;
-            if (dig > large) {
-                large = dig;
-            }
-            n /= 10;
-        }
-        return large;
-    }
-
-    public static void fibo(int n) {
-        int a =0;
-        int b =1;
-
-        for (int i = 0; i<n; i++) {
-            System.out.print( a + " ");
-            int c = a+b;
-            a = b;
-            b = c;
-        }
-    }
-
-    public static int fiboNth(int n) {
-        if(n == 0) {
-            return 0;
-        }
-        if(n == 1) {
-            return 1;
-        }
-        int a = 0;
-        int b = 1;
-        for (int i = 1; i < n; i++) {
-            int c = a+b;
-            a=b;
-            b=c;
-        }
-        return b;
-    }
-
-public static boolean isFascinating(int n) {
-        String str = "" + n + (2*n) + (3*n);
-
-        // Length is not 9 check
-        if (str.length() != 9) {
-            return false;
-        }
-    // Contains Zero check
-    for (int i = 0; i < str.length(); i++) {
-        if (str.charAt(i) == 0) {
-            return false;
-        }
-    }
-
-    for (char ch = '1'; ch<='9'; ch++) {
-        int count  = 0;
-        for (int k = 0; k < str.length(); k++) {
-            if (ch == str.charAt(k)) {
-                count++;
-            }
-        }
-        if (count != 1) {
-            return false;
-        }
-    }
-    return true;
-}
-
-public static int[] quickSort(int[] arr, int low , int high) {
-        // Base Case :
-    if (low >= high) {
-        return arr;
-    }
-
-    int start = low;
-    int end = high;
-    int mid = start + (end-start) / 2;
-    int pivot = arr[mid];
-
-    while (start <= end) {
-
-        while (arr[start] < pivot) {
-            start++;
-        }
-
-        while (arr[end] > pivot) {
-            end--;
-        }
-        // SWAP :
-        if (start <= end) {
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-            start++;
-            end--;
-        }
-    }
-    quickSort(arr, low, end);
-    quickSort(arr, start, high);
-
-    return arr;
-}
 
     public static void main(String[] args) {
-        int[] arrray = {10,40,20,80,60,50,30,200,2,5,7,1};
+        int[] array = {1,2,2,1};
         int[] arr = {60};
-        int n = arrray.length;
-        int[] new1 = quickSort(arrray, 0, arrray.length-1);
-        System.out.println(Arrays.toString(new1));
+        int n = array.length;
+        System.out.println(peakElement(array));
+
 
 
     }
