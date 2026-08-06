3896962005973521cea3a9f7cf225f6336e17916
niyamat <niyamatbarote@gmail.com>
2026-08-07 00:11:47 +0530

    test case for Q) 989

--- src/main/java/DSA/randomLeetCode/RandomPractice.java
+++ src/main/java/DSA/randomLeetCode/RandomPractice.java
@@ -204,6 +204,9 @@ public class RandomPractice {
         return prod-sum;
     }
 
+    // Test case for Q)989
+    //[1,2,6,3,0,7,1,7,1,9,7,5,6,6,4,4,0,0,6,3]
+
     public static void main(String[] args) {
         int[] array = {7,9,2,4};
         int[] arr = {60};
