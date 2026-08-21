a4c026e2341592acb6368b7edf81e543833f73f0
niyamat <niyamatbarote@gmail.com>
2026-08-21 23:59:47 +0530

    happy number

--- src/main/java/DSA/Numbers/HappyNumber.java
+++ src/main/java/DSA/Numbers/HappyNumber.java
@@ -1,7 +1,37 @@
 package DSA.Numbers;
 
 public class HappyNumber {
+
+
+    public static boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+
+        do {
+            slow = numSquare(slow);
+            fast = numSquare(numSquare(fast));
+        } while (slow != fast);
+
+        if (slow == 1) {
+            return true;
+        }
+
+        return false;
+
+    }
+
+    public static int numSquare(int n) {
+        int ans = 0;
+        while (n>0) {
+            int last = n%10;
+            ans += last * last;
+            n /= 10;
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         // is Number Happy Using Floyd's Cycle Detection Algorithm
+        System.out.println(isHappy(2));
     }
 }
